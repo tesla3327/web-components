1473cea74672e870637166813d29595806aef201
Michael Thiessen <michael.thiessen@vidyard.com>
2018-06-05 14:07:13 -0400

    Building DOM elements without a declaritive API is the worst

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 import ClickyBoi from './clicky-boi.js';
 import './counter-display.js';
 
+const createElement = (tag, opts, children) => {
+  const { props, attrs, events } = opts;
+  const element = document.createElement(tag);
+
+  for (let prop in props) {
+    element[prop] = props[prop];
+  }
+
+  for (let attr in attrs) {
+    element.setAttribute(attr, attrs[attr]);
+  }
+
+  for (let event in events) {
+    element.addEventListener(event, events[event]);
+  }
+
+  if (children && Array.isArray(children)) {
+    for (let child of children) {
+      element.appendChild(child);
+    }
+  }
+
+  return element;
+};
+
 class AppRoot extends HTMLElement {
   constructor() {
     super();
@@ -11,18 +36,7 @@ class AppRoot extends HTMLElement {
 
     this.shadow = this.attachShadow({ mode: 'closed' });
     this.styleNode = document.createElement('style');
-    this.counter = document.createElement('counter-display');
-
-    // Make some buttons
-    this.resetButton = ClickyBoi.make('Reset', () => this.setState({ count: 0 }));
-    this.incrementBtn = ClickyBoi.make('+', () => this.setState({ count: this.state.count + 1 }));
-    this.decrementBtn = ClickyBoi.make('-', () => this.setState({ count: this.state.count - 1 }));
-
     this.shadow.appendChild(this.styleNode);
-    this.shadow.appendChild(this.counter);
-    this.shadow.appendChild(this.decrementBtn);
-    this.shadow.appendChild(this.resetButton);
-    this.shadow.appendChild(this.incrementBtn);
   }
 
   get buttonText() {
@@ -35,8 +49,12 @@ class AppRoot extends HTMLElement {
     return `
       :host {
         display: flex;
-        flex-direction: column;
-        align-items: center;
+        justify-content: center;
+        width: 100%;
+      }
+
+      counter-display {
+        text-align: center
       }
     `;
   }
@@ -51,10 +69,49 @@ class AppRoot extends HTMLElement {
   }
 
   render() {
-    this.resetButton.text = this.buttonText;
+    const resetButton = createElement(
+      'clicky-boi',
+      {
+        props: {
+          text: this.buttonText
+        },
+        events: {
+          'button-click': () => this.setState({ count: 0 })
+        }
+      }
+    );
+
+    const incrementBtn = createElement('clicky-boi', {
+      props: { text: '+' },
+      events: { 'button-click': () => this.setState({ count: this.state.count + 1}) }
+    });
+
+    const decrementBtn = createElement('clicky-boi', {
+      props: { text: '-' },
+      events: { 'button-click': () => this.setState({ count: this.state.count - 1}) }
+    });
+
+    const counter = createElement('counter-display', {
+      props: { count: this.state.count }
+    });
+
+    const newChild = createElement('div', {}, [
+      this.styleNode,
+      counter,
+      createElement('div', {}, [
+        decrementBtn,
+        resetButton,
+        incrementBtn
+      ])
+    ]);
+
     this.styleNode.textContent = this.style;
-    this.counter.count = this.state.count;
-    this.decrementBtn.disabled = this.state.count === 0;
+
+    if (this.shadow.children[0]) {
+      this.shadow.replaceChild(newChild, this.shadow.children[0]);
+    } else {
+      this.shadow.appendChild(newChild);
+    }
   }
 }
 
